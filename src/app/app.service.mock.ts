95b632cdb7b789f804990eccdb7b77b527da19bd
Алексей Лазуткин <rabota_alexey@mail.ru>
2024-01-28 23:10:54 +0300

    Hint

--- src/app/app.service.mock.ts
+++ src/app/app.service.mock.ts
@@ -8,6 +8,7 @@ export class WorkerService {
   private worker: Worker;
 
   constructor() {
+    // Also we can use SharedWorker, but it's not in test issue
     this.worker = new Worker(new URL('./data.worker', ''), {
       type: 'module',
     });
